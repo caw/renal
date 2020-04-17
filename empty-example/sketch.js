ebcecbf9df3ab0b96deafea5bbcb9de540e63bd0
Chris Wright <cawright.99@gmail.com>
2020-04-17 19:15:38 +1000

    first remote commit

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,7 +1,41 @@
+const w = 800;
+const h = 600;
+
+class Molecule {
+  constructor() {
+    this.diameter = 15;
+  }
+  draw() {
+    fill(this.color)
+    circle(this.x, this.y, this.diameter)
+  }
+}
+
+class RedMolecule extends Molecule {
+  constructor() {
+    super()
+    this.color = "red"
+    this.x = 400;
+    this.y = 150;
+  }
+}
+
+const r = new RedMolecule()
+
 function setup() {
-  // put setup code here
+  createCanvas(w, h);
+  background(0);
+  fill(204);
+  stroke(0);
+  rect(100, 100, 600, 100);
+  stroke("red");
+  strokeWeight(4);
+  line(100, 100, 700, 100);
+  line(100, 200, 700, 200);
+
 }
 
 function draw() {
-  // put drawing code here
+  r.draw();
+
 }
